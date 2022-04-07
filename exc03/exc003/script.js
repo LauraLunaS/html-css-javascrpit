86390870bfd8e0bce4dff126d723e48fe8cfbee9
Laura Luna <lauraluna.siqueira@hotmail.com>
2022-04-07 05:15:51 -0300

    submit

--- exc03/exc003/script.js
+++ exc03/exc003/script.js
@@ -1,8 +1,8 @@
-let nome = prompt("Digite seu nome: ");
+let nome = prompt('Digite seu nome: ');
 
 while (nome === "") {
-    prompt('Digite seu nome corretamente: ')
     nome = prompt("Digite seu nome: ");
 }
 
 alert(`Olá, ${nome}`);
+
